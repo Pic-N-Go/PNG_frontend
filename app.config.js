7e0458a302f80d270b42bb85f978f9140155cb05
Lucy <lucy.park@traport.com>
2026-06-26 14:30:13 +0900

    fix(ios): inject KAKAO_APP_KEY via app.config.js instead of hardcoding in Info.plist

---
+++ app.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  expo: {
+    name: 'PNG',
+    slug: 'png',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/images/logo/logo_2.png',
+    userInterfaceStyle: 'light',
+    newArchEnabled: true,
+    splash: {
+      image: './assets/images/logo/logo_2.png',
+      resizeMode: 'contain',
+      backgroundColor: '#E31B59',
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.picngo.app',
+      infoPlist: {
+        KAKAO_APP_KEY: process.env.EXPO_PUBLIC_KAKAO_NATIVE_APP_KEY,
+      },
+    },
+    android: {
+      package: 'com.picngo.app',
+      adaptiveIcon: {
+        foregroundImage: './assets/images/logo/logo_2.png',
+        backgroundColor: '#ffffff',
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+    },
+    web: {
+      favicon: './assets/images/logo/logo_2.png',
+    },
+    plugins: [],
+  },
+};
