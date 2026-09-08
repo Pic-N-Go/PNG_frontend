cc29d237fc3162faf2db50c40838e039c04caaa2
Youngjae so <terranbang@gmail.com>
2026-09-08 19:47:58 +0900

    fix: 팔로우, 팔로잉 목록의 사용자 프로필 이동 경로 수정

--- src/navigation/stacks/MyPageStack.tsx
+++ src/navigation/stacks/MyPageStack.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MyPageScreen from '@/screens/mypage/MyPageScreen';
-import UserProfileScreen from '@/screens/mypage/UserProfileScreen';
 import SettingScreen from '@/screens/mypage/SettingScreen';
 import BookmarkedSpotListScreen from '@/screens/mypage/BookmarkedSpotListScreen';
 import NotificationScreen from '@/screens/mypage/NotificationScreen';
@@ -24,7 +23,6 @@ export type MyPageStackParamList = {
   MyPage: undefined;
   // collectionId: MY 탭 컬렉션 줄에서 그 컬렉션만 선택된 상태로 진입한다.
   BookmarkedSpotList: { collectionId?: number } | undefined;
-  UserProfile: { userId: string };
   // openThemeSheet: 홈 "관심 스팟" 안내에서 바로 관심 테마 시트를 열며 진입한다.
   Setting: { openThemeSheet?: boolean } | undefined;
   ProfileEdit: undefined;
@@ -51,7 +49,6 @@ export default function MyPageStack() {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="MyPage" component={MyPageScreen} />
       <Stack.Screen name="BookmarkedSpotList" component={BookmarkedSpotListScreen} />
-      <Stack.Screen name="UserProfile" component={UserProfileScreen} />
       <Stack.Screen name="Setting" component={SettingScreen} />
       <Stack.Screen name="Notification" component={NotificationScreen} />
       <Stack.Screen name="Inquiry" component={InquiryListScreen} />
